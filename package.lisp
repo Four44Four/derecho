e40065ad19d8860050136ab5776ec81c68bd37e7
uhhh <no>
2026-09-18 23:49:11 -0700

    removed leftover commented out junk from testing

--- package.lisp
+++ package.lisp
@@ -1,34 +1,4 @@
 (defpackage #:derecho
   (:use #:cl)
   (:nicknames :drch)
-  (:export #:request
-   ;; #:make-client
-   ;; #:client-loop
-   ;; #:run
-   ;; #:stop
-
-   ;; #:request
-   ;; #:get
-   ;; #:post
-   ;; #:put
-   ;; #:patch
-   ;; #:delete
-   ;; #:head
-   ;; #:options
-   ;; #:trace
-   ;; #:query
-
-   ;; #:response
-   ;; #:response-status
-   ;; #:response-headers
-   ;; #:response-body
-   ;; #:response-http-version
-
-   ;; #:request-handle
-   ;; #:cancel-request
-
-   ;; #:http-client-error
-   ;; #:http-client-timeout
-   ;; #:http-client-connection-error
-   ;; #:http-client-tls-error
-    ))
+  (:export #:request))
